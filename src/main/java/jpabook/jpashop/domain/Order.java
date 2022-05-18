da7463a11bacb62a1eadaf759c5788578ff9a4c8
gyubum <sgb8170@naver.com>
2022-05-19 00:47:36 +0900

    엔티티 클래스 개발

---
+++ src/main/java/jpabook/jpashop/domain/Order.java
@@ -0,0 +1,38 @@
+package jpabook.jpashop.domain;
+
+import lombok.Getter;
+import lombok.Setter;
+
+import javax.persistence.*;
+import java.time.LocalDateTime;
+import java.util.ArrayList;
+import java.util.List;
+
+@Entity
+@Table(name = "orders")
+@Getter @Setter
+public class Order {
+
+    @Id @GeneratedValue
+    @Column(name = "order_id")
+    private Long id;
+
+    // 연관관계 주인
+    @ManyToOne
+    @JoinColumn(name = "member_id") // 외래키 설정
+    private Member member;
+
+    // 연관관계 주인이 아님을 의미
+    @OneToMany(mappedBy = "order")
+    private List<OrderItem> orderItems = new ArrayList<>();
+
+    @OneToOne
+    @JoinColumn(name = "delivery_id")
+    private Delivery delivery;
+
+    private LocalDateTime orderDate; // 주문 시간
+
+    @Enumerated(EnumType.STRING)
+    private OrderStatus status; // 주문 상태
+
+}
